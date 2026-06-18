c60532350ebd1e6e419737e5f44d213804320865
hendra <148732074+hndraa@users.noreply.github.com>
2026-06-19 07:40:43 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+@file:Suppress("DEPRECATION")
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io")
+        maven { url = uri("https://s3.amazonaws.com/repo.commonsware.com") }
+        }
+    }
+}
+
+rootProject.name = "ProjectMobiles"
+include(":app")
